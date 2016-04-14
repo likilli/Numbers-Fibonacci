02ba5f2c9d79c311b0b780bed845746534ce4ecf
Olimpiada Lysenko <olimpiadalys@gmail.com>
2016-04-14 22:36:25 +0300

    add correction
    add correction

--- Fibonacci.js
+++ Fibonacci.js
@@ -4,8 +4,6 @@
 //1 2 3 4 5 6  7 
 6..fibo//13
 
-
-///////////
 'use strict'
 
 Object.defineProperty(Number.prototype, "fibo",
@@ -16,7 +14,7 @@ Object.defineProperty(Number.prototype, "fibo",
      	return 1
 	}
 })
-//////////е
+//////////
 
 function fibo(num)
 {
